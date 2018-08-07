cae5d39aadb8670f6dc9bc51e68de2f171da4f15
Tudor <tudorcebere@gmail.com>
2018-08-07 10:09:21 +0300

    working monad

--- monadic_parser.hs
+++ monadic_parser.hs
@@ -1,55 +1,117 @@
-import Prelude hiding ((>>=), return)
 import Data.Maybe
 import Data.Char
+import Control.Applicative 
+import Control.Monad (liftM, ap)
 
 newtype M a = M (String -> Maybe (a, String))
 
+instance Functor M where
+  fmap = liftM
 
-class ParserMonad m where
-	--monad logic
-    return :: a -> m a
-    (>>=) :: m a -> (a -> m b) -> m b
-    get_content :: m a -> (String -> Maybe (a, String))
-    (.||.) :: m a -> m a -> m a
-    --regex logic
-    star :: m a -> m [a]
-    plus :: m a -> m [a]
-    --parser logic
-    sat :: (Char -> Bool) -> m Char
-    charp :: Char -> m Char
-	    
+instance Applicative M where
+  pure  = return
+  (<*>) = ap
+
+instance Monad M where    
+    return x = M (\s -> Just (x, s))
+    (>>=) (M p) f =  (M (\s -> case (p s) of
+                        Just (v, s') -> ((get_content (f v)) s')
+                        Nothing -> Nothing))
 
 
-instance ParserMonad M where	
-	return x = M (\s -> Just (x, s))
 
-	(>>=) (M p) f =  (M (\s -> case (p s) of
-						Just (v, s') -> ((get_content (f v)) s')
-						Nothing -> Nothing))
+star :: M a -> M [a]
+star p = plus p .||. return []
 
-	(.||.) (M p)  (M p') = M (\s -> case p s of
-					Just (v, s') -> Just (v, s')
-					Nothing      -> p' s)
+plus :: M a -> M [a]
+plus p = p >>=  (\x -> star p >>= (\y -> return (x:y)))
 
-	get_content (M var) = var
-	
-	plus p = p >>=  (\x -> star p >>= (\y -> return (x:y)))
-	
-	star p = plus p .||. return []
+--beta ideas
+and_one :: M a ->  M a -> M [a]
+and_one p p' = p >>= (\x -> p' >>= (\y -> return (x:[y])))
 
- 	sat p = M (\s -> case s of 
- 					[] -> Nothing
- 					x  -> case (p (head x)) of 
- 							True -> Just ((head x), (tail x))
- 							False -> Nothing)
+and_multiple :: M [a] -> M a -> M [a]
+and_multiple p p' = p >>= (\x -> p' >>= (\y -> return (x ++ [y])))
 
- 	charp c = (sat (== c))
+--string :: String -> M String 
+--string str =  
+--    where 
+--        char_parsers = (map (charp) str)
 
+sat :: (Char -> Bool) -> M Char
+sat p = M (\s -> case s of 
+                    [] -> Nothing
+                    x  -> case (p (head x)) of 
+                            True -> Just ((head x), (tail x))
+                            False -> Nothing)
+
+(.||.) :: M a -> M a -> M a
+(.||.) (M p)  (M p') = M (\s -> case p s of
+                    Just (v, s') -> Just (v, s')
+                    Nothing      -> p' s)
+
+charp :: Char -> M Char
+charp c = (sat (== c))
+
+get_content :: M a -> (String -> Maybe (a, String))
+get_content (M var) = var
+    
 whitespace :: M String
 whitespace = (star (charp ' '))
 
 alphanumeric :: M String
-alphanumeric = (star (sat isAlpha))
+alphanumeric = (star (sat isAlpha))   
 
-whitespace_and_alphanumeric :: M String
-whitespace_and_alphanumeric = alphanumeric 
+numeric :: M String
+numeric = (star (sat isNumber))
+
+variable_parser :: M String
+variable_parser = do {
+    e  <- alphanumeric;    
+    whitespace;
+    return e
+}
+
+validate_number :: String -> Bool
+validate_number [] = True
+validate_number (x:xs) = if (isNumber x)
+    then validate_number xs
+    else False
+
+numeric_parser :: M String
+numeric_parser = do {
+    e <- numeric;
+    whitespace;
+    return e 
+}
+
+string :: String -> M String
+string (x:xs) = (charp x) >>= (\c -> string xs >>= (\str -> return (c:str))) 
+string [] = whitespace
+
+-- bTrue :: M BExp
+-- bTrue = do {
+--     string "True";
+--     return (BVal True)
+-- }
+
+-- bFalse :: M BExp
+-- bFalse = do {
+--     string "False";
+--     return (BVal False)
+-- }
+
+-- bAnd :: M BExp 
+-- bAnd = do {
+--     bterm1 <- bFalse .||. bTrue;
+--     op <-string "&&";
+--     bterm2 <- ebFalse .||. bTrue;
+--     return (BOperation bterm1 And bterm2)
+-- }
+
+
+
+-- bExp :: BExp 
+-- bExp = do {
+    
+-- }
