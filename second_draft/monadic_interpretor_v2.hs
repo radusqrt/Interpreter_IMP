606b26511097e995acc68c1c9983f9b616923c65
Tudor <tudorcebere@gmail.com>
2018-09-29 19:37:07 +0300

    nonmonadic

--- second_draft/monadic_interpretor_v2.hs
+++ second_draft/monadic_interpretor_v2.hs
@@ -37,13 +37,12 @@ interpretor ((Item_AExp (AValue v')):(OpL v):(Aritm op):xs,m) = (((Item_AExp (AO
 interpretor ((Item_AExp (AString str)):xs,m) = (((Item_AExp (AValue (m M.! str))):xs),m)
 --interpretor ((Item_AExp  e):xs,m) = (xs, m)
 
---am facut doar pentru AND/Greater, dar pot generaliza pentru orice operator prin stiva
+-- pun si operatia pe stiva, o scot doar la final (fata de exemplul de mai sus)
 interpretor ((Item_BExp (BOperation (BValue v1) And (BValue v2))):xs, m) = ((Item_BExp (BValue (v1 && v2))):xs,m)
 interpretor ((Item_BExp (BOperation e1 And e2)):xs, m) = ((Item_BExp e1):(BOpR e2):xs, m)
 interpretor ((Item_BExp (BValue v1)):(BOpR e2):xs, m)  = ((Item_BExp e2):(BOpL (BValue v1)):xs, m)
 interpretor ((Item_BExp (BValue v2)):(BOpL v1):xs, m)  = ((Item_BExp (BOperation v1 And (BValue v2))):xs, m)
 
--- --pun op-ul pe stiva, model pentru cele de mai sus
 interpretor ((Item_BExp (BCompare (AValue v1) Greater (AValue v2))):xs, m) = ((Item_BExp (BValue (v1 > v2))):xs,m)
 interpretor ((Item_BExp (BCompare e1 op e2)):xs, m) = ((Item_AExp e1):(OpR e2):(BArim op):xs,m)
 interpretor ((Item_AExp (AValue v1)):(OpR e2):(BArim op):xs, m) = ((Item_AExp e2):(OpL (AValue v1)):(BArim op):xs,m)
@@ -51,6 +50,7 @@ interpretor ((Item_AExp (AValue v2)):(OpL v1):(BArim op):xs, m)  = ((Item_BExp (
 
 interpretor ((Item_AST (Init str ast)):xs, m)   = ((Item_AST ast):xs, (set_values str m))
 interpretor ((Item_AST (Asign str aexp)):xs, m) = ((Item_AExp aexp):(Var str):xs,m)
+interpretor ((Item_AExp (AValue v1)):(Var str):xs, m) = (xs, (M.insert str v1 m))
 
 interpretor (((Item_AST (If (BValue True) p p')):xs),m)  = ((Item_AST p):xs,m)
 interpretor (((Item_AST (If (BValue False) p p')):xs),m) = ((Item_AST p'):xs,m)
